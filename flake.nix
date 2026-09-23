4e3e50b23a8a04726a5bc97cb33f56b9b1b32c5a
Koki Shigekuni <shigekuni.work@gmail.com>
2026-09-23 18:46:56 +0900

    Setup Nix and moonrepo (#5)
    * feat: setup nix
    
    * feat: setup moonrepo

---
+++ flake.nix
@@ -0,0 +1,26 @@
+{
+  description = "file-box development environment";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in {
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [
+            go_1_27
+            moon
+            buf
+            atlas
+            golangci-lint
+            govulncheck
+            lefthook
+          ];
+        };
+      });
+}
